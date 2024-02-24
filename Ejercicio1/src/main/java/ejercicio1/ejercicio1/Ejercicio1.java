dfc4307877bf571a338c3943f414d737b6432cf3
CamiloPJ <126632936+CamiloPJ@users.noreply.github.com>
2024-02-24 18:40:28 -0500

    Add files via upload

---
+++ Ejercicio1/src/main/java/ejercicio1/ejercicio1/Ejercicio1.java
@@ -0,0 +1,16 @@
+package ejercicio1.ejercicio1;
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        int edjuan=9;
+        int edana, edalber, edmama;
+        edalber=(2*edjuan)/3;
+        edana=(4*edjuan)/3;
+        edmama=edjuan+edalber+edana;
+        
+        System.out.println("La edad de Juan es: "+ edjuan);
+        System.out.println("La edad de Ana es: "+ edana);
+        System.out.println("La edad de Alber es: "+ edalber);
+        System.out.println("La edad de la Mamá es: "+ edmama);
+    }
+}
